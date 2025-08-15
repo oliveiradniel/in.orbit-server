d6368d9418e0437c497661e876d9dd9531e9af58
oliveiradniel <kadadniel@gmail.com>
2025-08-15 10:57:41 -0300

    chore: add PrismaService and DatabaseModule

--- src/modules/goals/goals.service.ts
+++ src/modules/goals/goals.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from 'src/shared/database/prisma.service';
+
 @Injectable()
 export class GoalsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   async findWeekGoals() {}
 
   async create() {}
