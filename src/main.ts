82acad4c13644a436e7b13ffb71ad6ded093fb1d
oliveiradniel <kadadniel@gmail.com>
2025-08-18 17:24:39 -0300

    chore: enable CORS using FRONTEND_ORIGIN from config

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { getConfig } from './shared/config/config.helper';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = getConfig(app.get(ConfigService));
+
+  const origin = configService.FRONTEND_ORIGIN;
+
+  app.enableCors({
+    origin,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
