3a335075be0c85c9d5e883e54be100b9c8628b40
oliveiradniel <kadadniel@gmail.com>
2025-08-22 16:29:02 -0300

    docs: add API documentation with Swagger module

--- src/modules/goals-completed/dto/create-goal-completed.dto.ts
+++ src/modules/goals-completed/dto/create-goal-completed.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class CreateGoalCompletedDTO {
+  @ApiProperty({
+    example: 'cf6a0b3d-21f7-4cb0-a351-feb63fe3783d',
+    description: 'Goal id',
+  })
   @IsString()
   @IsUUID()
   @IsNotEmpty()
