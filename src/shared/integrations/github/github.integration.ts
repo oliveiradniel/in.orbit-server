444c73ea24a07599b0ef97a9bddf7c3faa851f9a
oliveiradniel <kadadniel@gmail.com>
2025-08-25 09:03:03 -0300

    feat: implement GitHub OAuth integration (access token + user fetch)

---
+++ src/shared/integrations/github/github.integration.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+
+import { ConfigService } from '@nestjs/config';
+
+import { AccessTokenResponse } from './interfaces/access-token-response.interface';
+import { UserResponse } from './interfaces/user-response.interface';
+
+@Injectable()
+export class GitHubIntegration {
+  constructor(private readonly configService: ConfigService) {}
+
+  async getAccessTokenFromCode(code: string) {
+    const accessTokenURL = new URL(
+      'https://github.com/login/oauth/access_token',
+    );
+
+    accessTokenURL.searchParams.set(
+      'client_id',
+      this.configService.get('GITHUB_CLIENT_ID')!,
+    );
+
+    accessTokenURL.searchParams.set(
+      'client_secret',
+      this.configService.get('GITHUB_CLIENT_SECRET')!,
+    );
+
+    accessTokenURL.searchParams.set('code', code);
+
+    const response = await fetch(accessTokenURL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`GitHub auth failed: ${response.statusText}`);
+    }
+
+    const { access_token } = (await response.json()) as AccessTokenResponse;
+
+    return access_token;
+  }
+
+  async getUserFromGitHubAccessToken(accessToken: string) {
+    const response = await fetch('https://api.github.com/user', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const data = (await response.json()) as UserResponse;
+
+    return data;
+  }
+}
