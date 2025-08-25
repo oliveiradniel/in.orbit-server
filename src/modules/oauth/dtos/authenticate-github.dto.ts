24886fce71eab320c5091ce72ca5e048e8044a9f
oliveiradniel <kadadniel@gmail.com>
2025-08-25 10:16:59 -0300

    docs: add Swagger documentation for GitHub login DTO and controller

--- src/modules/oauth/dtos/authenticate-github.dto.ts
+++ src/modules/oauth/dtos/authenticate-github.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthenticateGitHubDTO {
+  @ApiProperty({
+    example: '3c6fde8098eb6d0954d5',
+    description: 'Code provided by GitHub.',
+  })
   @IsString()
   @IsNotEmpty()
   code: string;
