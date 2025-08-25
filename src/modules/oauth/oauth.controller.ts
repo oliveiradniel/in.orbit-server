14ff2faf0145a8d0b2e81ae2cf9281c136c8079b
oliveiradniel <kadadniel@gmail.com>
2025-08-25 09:52:23 -0300

    feat: implement GitHub OAuth login flow (DTO, controller, service)

--- src/modules/oauth/oauth.controller.ts
+++ src/modules/oauth/oauth.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { OAuthService } from './oauth.service';
+
+import { AuthenticateGitHubDTO } from './dtos/authenticate-github.dto';
 
 @Controller('oauth')
 export class OAuthController {
   constructor(private readonly oauthService: OAuthService) {}
+
+  @Post('github')
+  githubAuthenticate(@Body() authenticateGitHubDTO: AuthenticateGitHubDTO) {
+    const { code } = authenticateGitHubDTO;
+
+    return this.oauthService.githubLogin(code);
+  }
 }
--- src/modules/oauth/oauth.service.ts
+++ src/modules/oauth/oauth.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { GitHubIntegration } from 'src/shared/integrations/github/github.integration';
+
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
-export class OAuthService {}
+export class OAuthService {
+  constructor(
+    private readonly githubIntegration: GitHubIntegration,
+    private readonly usersRepository: UsersRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async githubLogin(code: string) {
+    const githubAccessToken =
+      await this.githubIntegration.getAccessTokenFromCode(code);
+
+    const githubUser =
+      await this.githubIntegration.getUserFromGitHubAccessToken(
+        githubAccessToken,
+      );
+
+    const user = await this.usersRepository.getUserByExternalAccountId(
+      githubUser.id,
+    );
+
+    let userId: string | null;
+
+    if (user) {
+      userId = user.id;
+    } else {
+      const createdUser = await this.usersRepository.create({
+        name: githubUser.name,
+        email: githubUser.email,
+        avatarURL: githubUser.avatar_url,
+        externalAccountId: githubUser.id,
+      });
+
+      userId = createdUser.id;
+    }
+
+    const payload = { sub: userId };
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { accessToken };
+  }
+}
