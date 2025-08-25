e12afbef2af19dba29e72943d9f9ab7bc536db6b
oliveiradniel <kadadniel@gmail.com>
2025-08-24 21:46:15 -0300

    chore: create OAuth module structure

---
+++ src/modules/oauth/oauth.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+
+import { OauthService } from './oauth.service';
+
+@Controller('oauth')
+export class OauthController {
+  constructor(private readonly oauthService: OauthService) {}
+}
