189264cee41549fb5b0b812a3b1c3cc6bc59a6cc
oliveiradniel <kadadniel@gmail.com>
2025-08-25 13:44:09 -0300

    feat: add decorator to declare public route and implement verification in AuthGuard

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -5,26 +5,38 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 
 import { Request } from 'express';
 
 import { getConfig } from 'src/shared/config/config.helper';
+import { IS_PUBLIC_KEY } from 'src/shared/decorators/is-public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private configService: ConfigService,
     private jwtService: JwtService,
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic =
+      this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]) ?? false;
+
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest<Request>();
 
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
     }
+
     try {
       const { JWT_SECRET } = getConfig(this.configService);
 
