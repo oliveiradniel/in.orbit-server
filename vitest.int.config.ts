0dfbbe9c8ae567c5d8a2169fc651ba9519e2e067
oliveiradniel <kadadniel@gmail.com>
2025-09-03 16:56:26 -0300

    test: ensure integration tests use .env.test

--- vitest.int.config.ts
+++ vitest.int.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vitest/config';
 
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env.test' });
+
 export default defineConfig({
   test: {
     include: ['src/modules/**/*.int.spec.ts'],
