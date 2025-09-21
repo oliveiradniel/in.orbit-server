2912b408eac8bd22acf44a67b9b925dee0338dd5
oliveiradniel <kadadniel@gmail.com>
2025-09-21 19:51:35 -0300

    fix: add CONFIG_SERVICE provider to module

--- src/modules/oauth/oauth.module.ts
+++ src/modules/oauth/oauth.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { OAuthService } from './oauth.service';
 
 import { OAuthController } from './oauth.controller';
-import { OAUTH_SERVICE } from 'src/shared/constants/tokens';
+
+import { CONFIG_SERVICE, OAUTH_SERVICE } from 'src/shared/constants/tokens';
 
 @Module({
   imports: [],
   controllers: [OAuthController],
-  providers: [{ provide: OAUTH_SERVICE, useClass: OAuthService }],
+  providers: [
+    { provide: OAUTH_SERVICE, useClass: OAuthService },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    { provide: CONFIG_SERVICE, useClass: ConfigService },
+  ],
 })
 export class OAuthModule {}
