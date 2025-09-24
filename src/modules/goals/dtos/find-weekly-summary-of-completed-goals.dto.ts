97d9e3f0f626f4a3d98c76ab0651322ce2e499d4
oliveiradniel <kadadniel@gmail.com>
2025-09-24 12:22:10 -0300

    fix: adjust weekStartsAt transform in weekly summary DTO

--- src/modules/goals/dtos/find-weekly-summary-of-completed-goals.dto.ts
+++ src/modules/goals/dtos/find-weekly-summary-of-completed-goals.dto.ts
@@ -1,15 +1,11 @@
 import { Transform } from 'class-transformer';
 import { IsDate, IsOptional } from 'class-validator';
-import dayjs from 'dayjs';
 
 export class FindWeeklySummaryOfCompletedGoalsDTO {
   @IsDate()
   @IsOptional()
-  @Transform(({ value }: { value: string }) => {
-    if (!value) {
-      return dayjs().startOf('week').startOf('day').toDate();
-    }
-    return new Date(value);
-  })
+  @Transform(({ value }: { value: string }) =>
+    value ? new Date(value) : undefined,
+  )
   weekStartsAt: Date;
 }
