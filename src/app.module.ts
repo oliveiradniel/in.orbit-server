b0f3bcbe17335e0d35bfd67935463d599b27fa13
oliveiradniel <kadadniel@gmail.com>
2025-10-18 15:33:52 -0300

    chore: remove script loading for Dockerfile in production

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,6 @@ import { UsersModule } from './modules/users/users.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env.development',
       validate: validate,
     }),
     GoalsModule,
